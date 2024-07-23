3c0a1a054792beedc34479dc5d26fa6a0f90c988
Holly Smith <hollyschuldt@gmail.com>
2024-07-22 21:19:38 -0500

    fixed views and updated maintenance model

--- greentransportapi/models/maintenance.py
+++ greentransportapi/models/maintenance.py
@@ -3,5 +3,5 @@ from .scooter import Scooter
 
 class Maintenance(models.Model):
     scooter = models.ForeignKey(Scooter, on_delete=models.CASCADE)
-    maintenance_date = models.DateTimeField()
+    maintenance_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
